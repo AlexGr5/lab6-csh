9005287f72a977ae9e3a82eb9be22ee38c092c68
Alex <alex_gr2@mail.ru>
2021-10-16 18:23:41 +0700

    Добавьте файлы проекта.

---
+++ lab6-csh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab6_csh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
